34ec9d13483d71f1c85e8134f5571c8dd52fea80
Olumide Adekunle B. <116625009+olumideadekunle@users.noreply.github.com>
2025-11-25 10:53:57 +0100

    update picture

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Sidebar Navigation", layout="wide")
 
 # Fixed header (always visible and does not change)
-st.image("Olumide Buari.- Researcher.jpg", width=60)
+st.image("Olumide Buari- Researcher.jpg")
+#st.image("**Olumide Buari- Researcher.jpg**")
 
 
 st.markdown(
